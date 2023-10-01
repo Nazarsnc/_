3f8b70f08de37ba7f092965597ad8bba4ffc2607
Nazarsnc <143934973+Nazarsnc@users.noreply.github.com>
2023-10-01 12:47:08 +0300

    Update main.pyhjf

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("hell")
+from Pyot5.QtCore import Qt
+
+from Pyot5.QtWidgets import (QApplication, QWidget.
+
+Le CHBoxLayout, QVBoxLayout. (RadioButton)
+
+app= QApplication([])
+
+main win QWidget()
+
+main_win.setWindowTitle("Iestyna)
+
+main_win.resize(400,400)
+
+gtext QLabel (A)
+
+v_Line QVBoxLayout()
+
+viegladioButton(1991)
+
+v2-QRadioButton(1989)
+
+v3-QRadioButton(1998)
+
+v4-QRadioButton(1992)
+
+Layout1QHBoxLayout()
+
+Layout2gBoxLayout()
+
+LayoutBoxLayout()
+
+Layout.addWidget(qtext Lagonegt.Aligncenter) Layout2.addWidget(v1, ment Qt.Aligncenter) Layout2.addieget (v2, alterantoge Aligncenter
